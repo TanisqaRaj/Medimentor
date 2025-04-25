25ab031065770486f55e663102bfe6c5a5db15b4
TanisqaRaj <rajtanisqa@gmail.com>
2025-04-25 22:30:42 +0530

    Responsive UI for Admin sidebar completed

--- Frontend/src/components/Admin/AdminSidebar.jsx
+++ Frontend/src/components/Admin/AdminSidebar.jsx
@@ -17,20 +17,20 @@ const AdminSidebar = () => {
         { title: "Registered Doctor", icon: <FaUserDoctor />, path:"/doctorlist" },
         { title: "registered User", icon: <FaUser />, path:"/userlist" },
         { title: "Verify Doctor", icon: <FaUserCheck />, path: "https://www.nmc.org.in/information-desk/indian-medical-register/" },
-        { title: "Buy Medicine", icon: <AiTwotoneMedicineBox />, path:"/pharmacy" },
+        { title: "Dashboard", icon: <FaBookMedical /> ,path:"/admindashboard"},
       ];
 
   return (
       <div
         className={`${
           open ? "w-[250px]" : "w-[60px]"
-        } duration-400 p-4 flex flex-col h-[100vh] bg-emerald-500 text-black rounded-r-xl  justify-start sticky top-0`}
+        } duration-400 px-0.5 py-1.5 s:p-4 flex flex-col pb-10 bg-emerald-500 text-black rounded-r-xl  justify-start sticky top-0`}
       >
         {/* Toggle Icon */}
         <div
           className={`${
             !open && "rotate-180 justify-center"
-          } flex justify-end mb-6 cursor-pointer`}
+          } flex justify-end mb-6 mr-2 cursor-pointer`}
           onClick={() => setOpen(!open)}
         >
           <BsLayoutSidebarInset />
@@ -39,7 +39,7 @@ const AdminSidebar = () => {
         {/* Profile Picture */}
         {open && (
           <div className="flex flex-col items-center mb-12">
-            <div className="h-24 w-24 border shadow-lg rounded-full overflow-hidden">
+            <div className=" h-24 w-24 border shadow-lg rounded-full overflow-hidden">
               <img
                 src={`data:image/png;base64,${user?.image}`}
                 className="h-full w-full object-cover"
@@ -63,7 +63,7 @@ const AdminSidebar = () => {
             >
               <div className="text-xl">{item.icon}</div>
               {open && (
-                <span className="text-lg  text-gray-800 font-medium"
+                <span className="text-sm sm:text-lg  text-gray-800 font-medium"
                 >
                   {item.title}
                 </span>
