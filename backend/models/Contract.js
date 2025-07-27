54807a96b1e83567f58d29f51856c877ddeb4831
TanisqaRaj <rajtanisqa@gmail.com>
2025-07-27 22:33:43 +0530

    Map changes

--- backend/models/Contract.js
+++ backend/models/Contract.js
@@ -9,28 +9,35 @@ const contractSchema = new mongoose.Schema(
     },
 
     meetingDetails: {
-        meetingId: { type: String, required: false, unique: true },
-        meetingPassword: { type: String, required: false },
-        meetingUrl: { type: String, required: false },
-        location: { type: String, required: false },
-    }
+      meetingId: { type: String, required: false, unique: true },
+      meetingPassword: { type: String, required: false },
+      meetingUrl: { type: String, required: false },
+      location: {
+        city: { type: String, required: false },
+        state: { type: String, required: false },
+        pincode: { type: String, required: false },
+        latitude: { type: String, required: false },
+        longitude: { type: String, required: false },
+      },
+    },
   },
   {
     timestamps: true,
   }
 );
 
-contractSchema.pre('save', function (next) {
+contractSchema.pre("save", function (next) {
   if (!this.meetingDetails) this.meetingDetails = {};
 
   if (!this.meetingDetails.meetingId) {
     const date = new Date();
-    this.meetingDetails.meetingId = `${this.appointmentId}${date.toISOString().replace(/[^0-9]/g, "")}`;
+    this.meetingDetails.meetingId = `${this.appointmentId}${date
+      .toISOString()
+      .replace(/[^0-9]/g, "")}`;
     console.log("Generated meeting ID:", this.meetingDetails.meetingId);
   }
 
   next();
 });
-
 
 export default mongoose.model("Contract", contractSchema);
